f47b895ab96eb8c5fc8a0eda1e3769d46c8a86e6
Philipp A <flying-sheep@web.de>
2016-11-03 18:42:23 +0100

    added static properties

--- src/client/js/components/chapter.js
+++ src/client/js/components/chapter.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
 export default class Chapter extends React.Component {
+	static defaultProps = {
+		chapter: null
+	}
 	render() {
 		return <webview src={this.props.chapter}/>
 	}
-}
-Chapter.defaultProps = {
-	chapter: null
 }
--- src/client/js/components/toc.js
+++ src/client/js/components/toc.js
@@ -1,24 +1,24 @@
 import React from 'react'
 
 export default class Toc extends React.Component {
+	static defaultProps = {
+		nodes: []
+	}
 	render() {
 		return <ul>
 			{ this.props.nodes.map(toc => <TocNode toc={toc}/>) }
 		</ul>
 	}
 }
-Toc.defaultProps = {
-	nodes: []
-}
 
 export class TocNode extends React.Component {
+	static defaultProps = {
+		toc: []
+	}
 	render() {
 		return <div>
 			<header>{ this.props.toc.label }</header>
 			{ (this.props.toc.sub) ? <Toc nodes={this.props.toc.sub}/> : null }
 		</div>
 	}
-}
-TocNode.defaultProps = {
-	toc: []
 }

fc7516bf8026bf85349d97352ee0173c6bf5c2d0
Philipp A <flying-sheep@web.de>
2016-11-11 20:21:19 +0100

    improve webview type

--- global.d.ts
+++ global.d.ts
@@ -6,41 +6,47 @@ declare namespace NodeJS {
 
 declare module 'react-electron-webview' {
     interface WebViewProps extends React.HTMLProps<Electron.WebViewElement> {
-        src: string,
+        src: string
         
-        autosize?: boolean,
-        disablewebsecurity?: boolean,
-        httpreferrer?: string,
-        nodeintegration?: boolean,
-        plugins?: boolean,
-        preload?: string,
+        autosize?: boolean
+        disablewebsecurity?: boolean
+        httpreferrer?: string
+        nodeintegration?: boolean
+        plugins?: boolean
+        preload?: string
         useragent?: string
+        partition?: string
+        allowpopups?: boolean
+        webpreferences?: string
+        blinkfeatures?: string
+        disableblinkfeatures?: string
+        guestinstance?: string
         
-        loadCommit?: EventListener,
-        didFinishLoad?: EventListener,
-        didFailLoad?: EventListener,
-        didFrameFinishLoad?: EventListener,
-        didStartLoading?: EventListener,
-        didStopLoading?: EventListener,
-        didGetResponseDetails?: EventListener,
-        didGetRedirectRequest?: EventListener,
-        domReady?: EventListener,
-        pageTitleSet?: EventListener,
-        pageFaviconUpdated?: EventListener,
-        enterHtmlFullScreen?: EventListener,
-        leaveHtmlFullScreen?: EventListener,
-        consoleMessage?: EventListener,
-        newWindow?: EventListener,
-        close?: EventListener,
-        ipcMessage?: EventListener,
-        crashed?: EventListener,
-        gpuCrashed?: EventListener,
-        pluginCrashed?: EventListener,
-        destroyed?: EventListener,
+        loadCommit?: EventListener
+        didFinishLoad?: EventListener
+        didFailLoad?: EventListener
+        didFrameFinishLoad?: EventListener
+        didStartLoading?: EventListener
+        didStopLoading?: EventListener
+        didGetResponseDetails?: EventListener
+        didGetRedirectRequest?: EventListener
+        domReady?: EventListener
+        pageTitleSet?: EventListener
+        pageFaviconUpdated?: EventListener
+        enterHtmlFullScreen?: EventListener
+        leaveHtmlFullScreen?: EventListener
+        consoleMessage?: EventListener
+        newWindow?: EventListener
+        close?: EventListener
+        ipcMessage?: EventListener
+        crashed?: EventListener
+        gpuCrashed?: EventListener
+        pluginCrashed?: EventListener
+        destroyed?: EventListener
     }
     interface WebViewState {
-        loaded: boolean,
-        webview: Electron.WebViewElement,
+        loaded: boolean
+        webview: Electron.WebViewElement
     }
     export default class WebView extends React.Component<WebViewProps, WebViewState> {}  
 }

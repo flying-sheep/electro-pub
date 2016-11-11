9053a0139395fb2f911a32547df3bac3993c0420
Philipp A <flying-sheep@web.de>
2016-11-11 17:13:40 +0100

    switched to react-electron-webview

--- global.d.ts
+++ global.d.ts
@@ -4,10 +4,45 @@ declare namespace NodeJS {
     }
 }
 
-declare namespace JSX {
-    interface IntrinsicElements {
-        webview: { src: string },
+declare module 'react-electron-webview' {
+    interface WebViewProps {
+        src: string,
+        
+        autosize?: boolean,
+        disablewebsecurity?: boolean,
+        httpreferrer?: string,
+        nodeintegration?: boolean,
+        plugins?: boolean,
+        preload?: string,
+        useragent?: string
+        
+        loadCommit?: EventListener,
+        didFinishLoad?: EventListener,
+        didFailLoad?: EventListener,
+        didFrameFinishLoad?: EventListener,
+        didStartLoading?: EventListener,
+        didStopLoading?: EventListener,
+        didGetResponseDetails?: EventListener,
+        didGetRedirectRequest?: EventListener,
+        domReady?: EventListener,
+        pageTitleSet?: EventListener,
+        pageFaviconUpdated?: EventListener,
+        enterHtmlFullScreen?: EventListener,
+        leaveHtmlFullScreen?: EventListener,
+        consoleMessage?: EventListener,
+        newWindow?: EventListener,
+        close?: EventListener,
+        ipcMessage?: EventListener,
+        crashed?: EventListener,
+        gpuCrashed?: EventListener,
+        pluginCrashed?: EventListener,
+        destroyed?: EventListener,
     }
+    interface WebViewState {
+        loaded: boolean,
+        webview: Electron.WebViewElement,
+    }
+    export default class WebView extends React.Component<WebViewProps, WebViewState> {}  
 }
 
 declare module 'bitbar-webpack-progress-plugin' {
@@ -19,10 +54,4 @@ declare module 'bitbar-webpack-progress-plugin' {
 
 declare module 'postcss-cssnext' {
     
-}
-
-declare module 'autoprefixer' {
-    function autoprefixer(options?: any): any;
-    namespace autoprefixer {}
-    export = autoprefixer
 }
--- src/client/js/components/Chapter.tsx
+++ src/client/js/components/Chapter.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react'
+import WebView from 'react-electron-webview'
 
+import { AssetHandler } from '../../../app/handlers' 
 
 export interface ChapterProps {
 	chapter: string,
@@ -9,7 +11,18 @@ export default class Chapter extends React.Component<ChapterProps, {}> {
 	static defaultProps = {
 		chapter: null,
 	}
+	insertStyle = (e: Event) => {
+		const wvComponent = this.refs['webview'] as WebView
+		for (const sheet of ['style.css', 'fonts.css']) {
+			wvComponent.state.webview.executeJavaScript(`{
+				const s = document.createElement('style')
+				s.setAttribute('rel', 'stylesheet')
+				s.setAttribute('href', '${AssetHandler.prefix}/${ sheet }')
+				document.head.appendChild(s)
+			}`)
+		}
+	}
 	render() {
-		return <webview src={this.props.chapter} />
+		return <WebView ref="webview" src={this.props.chapter} domReady={this.insertStyle} />
 	}
 }

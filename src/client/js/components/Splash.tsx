9cbbd7e146a49ffbf58fd7b6c02bc3b3f9f06151
Philipp A <flying-sheep@web.de>
2016-11-11 17:19:18 +0100

    extracted splash

---
+++ src/client/js/components/Splash.tsx
@@ -0,0 +1,21 @@
+import { ipcRenderer } from 'electron'
+
+import * as React from 'react'
+import * as Dropzone from 'react-dropzone'
+
+export default class Splash extends React.Component<{}, {}> {
+	static onDrop(files: File[]) {
+		ipcRenderer.send('open', files[0].path)
+	}
+	render() {
+		return (
+			<div>
+				<p>Select a file from the list or open one</p>
+				<Dropzone onDrop={Splash.onDrop} multiple={false} className="drop-zone" activeClassName="active">
+					Drop an .epub file here<br/>
+					or click to select one
+				</Dropzone>
+			</div>
+		)
+	}
+}
--- src/client/js/main.tsx
+++ src/client/js/main.tsx
@@ -6,27 +6,10 @@ import { ipcRenderer } from 'electron'
 
 import * as React from 'react'
 import { render } from 'react-dom'
-import * as Dropzone from 'react-dropzone'
 
+import Splash from './components/Splash'
 import Toc from './components/Toc'
 import Chapter from './components/Chapter'
-
-class Splash extends React.Component<{}, {}> {
-	static onDrop(files: File[]) {
-		ipcRenderer.send('open', files[0].path)
-	}
-	render() {
-		return (
-			<div>
-				<p>Select a file from the list or open one</p>
-				<Dropzone onDrop={Splash.onDrop} multiple={false} className="drop-zone" activeClassName="active">
-					Drop an .epub file here<br/>
-					or click to select one
-				</Dropzone>
-			</div>
-		)
-	}
-}
 
 function renderChapter(toc: TableOfContents) {
 	render(<Chapter chapter={`${EPubHandler.prefix}/${toc.href}`}/>, document.getElementById('main'))
